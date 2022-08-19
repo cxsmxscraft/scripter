d967dd5b801a1f5625cd2583618f90dc084c2a08
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-19 17:50:31 +0500

    updating scripter

--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
-#!/bin/bash
-
-# shellcheck disable=SC2164
-cd "$(dirname "$0")"
 
 # Quick execute
 set -e
 
-function _install_help {
-    echo "usage ${0} [location]"
-    echo
-    echo "Scripts that installs the main script to given path. " 
-}
-
-function install {
-    echo "Installing the main script"
-    cp ./main.sh $1
-    DIRECTION="$(pwd)"
-    echo "$DIRECTION"
-}
-
-if [[ -z "${1}" ]]; then
-    _install_help
+if ! command -v git >/dev/null; then
+    echo "Error: git is required to install scipter." 1>&2
     exit 1
+fi
+
+scripter_uri="https://github.com/uwucraft/scripter.git"
+
+scripter_install="${SCRIPTER_INSTALL:-$HOME/.scripter}"
+bin_dir="$scripter_install/bin"
+exec="$bin_dir/scripter"
+
+
+git clone "$scripter_uri" "$scripter_install"
+
+echo "Scripter was installed successfully to $exec"
+if command -v scripter >/dev/null; then
+    echo "Run 'scripter help' to get started"
 else
-    install "${1}"
-    exit 0
+    case $SHELL in
+    /bin/zsh) shell_profile=".zshrc" ;;
+    *) shell_profile=".bash_profile" ;;
+    esac
+    echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+    echo "  export SCRIPTER_INSTALL=\"$scripter_install\""
+    echo "  export PATH=\"\$SCRIPTER_INSTALL/bin:\$PATH\""
+    echo "Run '$exec help' to get started"
 fi
