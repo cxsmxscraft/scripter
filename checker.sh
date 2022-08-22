e7d00ea4cad26e93b1bec7d9c17077b5a8c6dc17
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-22 16:57:37 +0500

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -1,6 +1,6 @@
 function checker {
-    # Check if server.properties or server.jar exists in the current directory
-    if [ ! -f server.properties ]; then
+    # Check if server.properties is present
+    if [ ! -f ./server.properties ]; then
         echo "This is not valid minecraft server directory. Try changing current working directory to your server..." 1>&2
         exit 1
     fi
